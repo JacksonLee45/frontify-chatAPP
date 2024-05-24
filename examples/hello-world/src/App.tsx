95751f1e46483981eb9050487eb9dd640f69284a
imoutaharik <ilham.moutaharik@frontify.com>
2024-05-24 10:49:47 +0200

    feat: add fondue components, styles and Frontify fonts to hello world app.

--- examples/hello-world/src/App.tsx
+++ examples/hello-world/src/App.tsx
@@ -1,7 +1,10 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import './App.css';
+import '@frontify/fondue/style';
 import { type PlatformAppContext, usePlatformAppBridge } from '@frontify/app-bridge';
 import { useEffect, useState } from 'react';
+import {Flex, FOCUS_VISIBLE_STYLE, Heading, IconArrowOutExternal20, merge} from "@frontify/fondue";
 
 export const App = () => {
     const appBridge = usePlatformAppBridge();
@@ -16,9 +19,31 @@ export const App = () => {
     }, [appBridge]);
 
     return (
-        <div className="flex h-[100vh] justify-center items-center flex-col">
-            A Frontify Platform App in React with tailwind.
-            <p className={'text-blue-500'}>Entrypoint: {context?.surface}</p>.
+        <div className="tw-font-body tw-h-screen tw-bg-base tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-y-6">
+            <Heading size="xx-large" weight="strong">Hello World!</Heading>
+            <Flex direction="column">
+                <p className="tw-text-text">Congratulations! You've Successfully Connected Your App to Our Platform.</p>
+                {
+                    context && (
+                        <p className="tw-text-text-weak tw-text-body-small">Surface: {context.surface}</p>
+                    )
+                }
+            </Flex>
+
+
+            <a
+                data-test-id="documentation-link"
+                href="https://developer.frontify.com/"
+                target="blank"
+                rel="noreferrer"
+                className={merge([
+                    FOCUS_VISIBLE_STYLE,
+                    'tw-bg-button-background tw-border-button-border hover:tw-bg-button-background-hover active:tw-bg-button-background-pressed  tw-group tw-border tw-box-box tw-relative tw-flex tw-gap-2 tw-items-center tw-justify-center tw-cursor-pointer tw-font-body tw-font-medium tw-rounded tw-px-4 tw-h-9 tw-text-body-medium',
+                ])}
+            >
+                <IconArrowOutExternal20/>
+                <span>Documentation</span>
+            </a>
         </div>
     );
 };
--- examples/hello-world/src/settings.ts
+++ examples/hello-world/src/settings.ts
@@ -1,12 +1,11 @@
 import { defineSettings } from '@frontify/platform-app';
 
-export const settings = defineSettings({
-    credentials: [
-        {
-            type: 'input',
-            id: 'single-line',
-            label: 'Input field',
-            defaultValue: 'Input Field Value',
-        },
-    ],
-});
+/**
+ * This file is where you can add settings to personalize the experience of the users.
+ * These settings will appear in the app details of the marketplace after deploying,
+ * publishing, and installing.
+ *
+ * For more information, please refer to the documentation:
+ * https://developer.frontify.com/document/2258#/getting-started/app-development-1/app-settings
+ */
+export const settings = defineSettings({});
